fbe4283607e5257b6112b01c696fa207eeda9db3
Jonas Silva <jonasverasilva@gmail.com>
2021-03-10 20:43:18 -0300

    start do projeto

---
+++ src/main/java/com/br/smallmanager/apismallManager/ApiSmallManagerApplication.java
@@ -0,0 +1,13 @@
+package com.br.smallmanager.apismallManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSmallManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSmallManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/smallmanager/apismallManager/ApiSmallManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.smallmanager.apismallManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSmallManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
