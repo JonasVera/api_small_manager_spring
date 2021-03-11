5038abecaef1c188abc7aa35fe7088317a32f4ac
Jonas Silva <jonasverasilva@gmail.com>
2021-03-10 21:23:21 -0300

    Criação da entidade de usuario

---
+++ src/main/java/com/br/smallmanager/apismallManager/dto/Usuario.java
@@ -0,0 +1,58 @@
+package com.br.smallmanager.apismallManager.dto;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Entity
+@Table(name = "usuario", schema = "api_smallmanager")
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+ 
+	@Id
+	@Column(name = "id_usuario")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	 
+	private Integer id_usuario;
+	@Column(name = "nome")
+	private String nome; 
+	@Column(name = "sobrenome")
+	private String sobrenome;
+	@Column(name = "data_nascimento")
+	private Date data_nacimento;
+	@Column(name = "sexo")
+	private String sexo;
+	@Column(name = "bio")
+	private String bio;
+	@Column(name = "contato_pessoal")
+	private String contato_pessoal; 
+	@Column(name = "img_login")
+	private String img_login;
+	@Column(name = "status_perfil")
+	private String status_perfil;
+	@Column(name = "email")
+	private String  email;
+	@Column(name = "confirma_email")
+	private boolean confirma_email;
+	@Column(name = "data_cadastro")
+	private Date data_cadastro; 
+	@Column(name = "data_atualizacao")
+	private Date data_atualizacao;
+	
+	
+}
