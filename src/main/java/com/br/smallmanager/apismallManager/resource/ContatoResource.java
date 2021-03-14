4dcbe8c47fb52ef2eff54fef39af479e692e3ca2
Jonas Silva <jonasverasilva@gmail.com>
2021-03-14 14:00:47 -0300

    implementação dos metodos do contato na classe de serviço

--- src/main/java/com/br/smallmanager/apismallManager/resource/ContatoResource.java
+++ src/main/java/com/br/smallmanager/apismallManager/resource/ContatoResource.java
@@ -1,5 +1,11 @@
 package com.br.smallmanager.apismallManager.resource;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contato")
 public class ContatoResource {
 
+	
 }
--- src/main/java/com/br/smallmanager/apismallManager/service/ContatoService.java
+++ src/main/java/com/br/smallmanager/apismallManager/service/ContatoService.java
@@ -1,5 +1,74 @@
 package com.br.smallmanager.apismallManager.service;
 
-public class ContatoService {
+import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.stereotype.Service;
+
+import com.br.smallmanager.apismallManager.entity.Contato;
+import com.br.smallmanager.apismallManager.entity.Empresa; 
+import com.br.smallmanager.apismallManager.exeptions.RegraNegocioException;
+import com.br.smallmanager.apismallManager.repository.ContatoRepository;
+
+@Service
+public class ContatoService {
+	
+	@Autowired
+	private ContatoRepository repository;
+	@Autowired
+	private EmpresaService empresaService;
+
+	@Transactional 
+	public Contato cadastrarContato(Empresa empresa,Contato contato) {
+		
+		Optional<Empresa> empresaExistente = empresaService.obterPorId(empresa.getId());
+		
+		if (!empresaExistente.isEmpty()) {
+			contato.setEmpresa(empresaExistente.get() );
+			return repository.save(contato);
+				
+		}else {
+			throw new RegraNegocioException("Não foi possivel cadastrar um contado, empresa não encontrada.");
+		}
+		 
+	}
+	
+	@Transactional 
+	public Optional<Contato> buscarPorId(Contato contato) {
+	 
+		 if	(contato.getEmpresa().getId() != null) {
+			 return repository.findById(contato.getId());
+		 }else {
+			 throw new RegraNegocioException("Contato não encontrado para esta empresa.");
+		 } 
+	}
+	 
+	@Transactional 
+	public Optional<Contato> obterPorId(Long id) {
+		return repository.findById(id);
+	}
+	@Transactional 
+	public List<Contato> buscarContatoEmpresa(Contato contatoFiltro) {
+		Example<Contato> example = Example.of(contatoFiltro, ExampleMatcher.matching()
+				.withIgnoreCase()
+				.withStringMatcher(StringMatcher.CONTAINING));
+		 
+		return repository.findAll(example);
+	}
+	
+	@Transactional 
+	public void excluirEmpresa(Contato contato) {
+		  repository.delete(contato);
+	 }
+		
+	@Transactional 
+	public void alterarEmpresa(Contato contato) {
+		repository.save(contato);
+	}
 }
