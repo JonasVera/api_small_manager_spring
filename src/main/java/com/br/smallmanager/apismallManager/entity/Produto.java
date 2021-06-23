e09fac28253cae30dcb3acb0fcb4b2786fe71c65
jonas.silva <jonas.silva@techwayinfo.com.br>
2021-06-23 08:05:11 -0300

    ajuste end point empresa

--- src/main/java/com/br/smallmanager/apismallManager/entity/Produto.java
+++ src/main/java/com/br/smallmanager/apismallManager/entity/Produto.java
@@ -5,17 +5,14 @@ import java.time.Instant;
 import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
+import javax.persistence.Table; 
+import com.fasterxml.jackson.annotation.JsonIgnore; 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -91,4 +88,7 @@ public class Produto {
 	@OneToMany(mappedBy = "produto" )  
 	private List<EventoAgenda> eventoAgenda;
 	 
+	@OneToMany(mappedBy = "produto")  
+	private List<Orcamento> orcamento;
+	
 }
